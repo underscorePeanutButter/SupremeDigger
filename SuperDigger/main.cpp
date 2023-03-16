9c9cc741c8c64509f52e03b16d894148ae3f4c35
Luke James <ljgecko1230@gmail.com>
2023-03-15 22:03:00 -0700

    add files

--- SuperDigger/main.cpp
+++ SuperDigger/main.cpp
@@ -1,14 +1,66 @@
-//
-//  main.cpp
-//  SuperDigger
-//
-//  Created by Lucas James on 2023-03-15.
-//
+// write shitty code ;-;
 
-#include <iostream>
+#include <cstdio>
+#include <SDL2/SDL.h>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+SDL_Window *window;
+SDL_Renderer *renderer;
+
+bool init();
+void cleanUp();
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+int main() {
+    if (!init()) {
+        return -1;
+    }
+    
+    SDL_Event e;
+    
+    bool running = true;
+    while (running) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+        
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+        SDL_RenderClear(renderer);
+        
+        SDL_RenderPresent(renderer);
+    }
+}
+
+bool init() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL initiailzation failure.\n");
+        return false;
+    }
+    
+    window = SDL_CreateWindow("Supreme Digger", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window creation failed.\n");
+        return false;
+    }
+    
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Renderer creation failed.\n");
+        return false;
+    }
+    
+    return true;
+}
+
+void cleanUp() {
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    window = NULL;
+    renderer = NULL;
+    
+    SDL_Quit();
 }
